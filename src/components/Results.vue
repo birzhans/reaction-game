b7dfdaa6f4c9aa3df9e533c739ebaf64dd31b815
Birzhan Serik <180107167@stu.sdu.edu.kz>
2021-06-12 15:37:46 +0600

    .

---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+	<p>Reaction time: {{ reactionTime }} ms</p>
+	<p class="rank">Rank: {{ rank }}</p>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'Results',
+	props: ['reactionTime'],
+
+	data() {
+		return {
+			rank: null
+		}
+	},
+
+	mounted() {
+		if (this.reactionTime < 250) { 
+			this.rank = "Fast!" 
+		} else if (this.reactionTime < 400) { 
+			this.rank = "Normal"
+		} else {
+			this.rank = "Snail pace..."
+		}
+
+	}
+}
+</script>
+
+<style>
+	.rank {
+		font-size: 1.4em;
+		color: #0faf87;
+		font-weight: bold;
+	}
+</style>
